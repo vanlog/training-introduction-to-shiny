d95606bbcc8754c330759f7a611a76ec70850197
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2021-05-06 09:03:05 +0200

    1.2 Change to Shinydashboard

--- dashboard/ui.R
+++ dashboard/ui.R
@@ -1,3 +1,15 @@
-ui <- fluidPage(
-  titlePanel("My Dashboard"),
+ui <- dashboardPage(
+  dashboardHeader(title = "My Dashboard"),
+
+  ## Sidebar content ----
+  dashboardSidebar(
+    sidebarMenu(
+
+    )
+  ),
+
+  ## Body content ----
+  dashboardBody(
+
+  )
 )
