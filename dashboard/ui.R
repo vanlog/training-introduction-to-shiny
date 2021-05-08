1c5ea55fef087ac1d8e8c834d232fb16c9ee2d7e
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2021-05-08 22:44:01 +0200

    1.6 compose the page

--- dashboard/ui.R
+++ dashboard/ui.R
@@ -16,12 +16,29 @@ ui <- dashboardPage(
     ## Analysis content --------------------------------------------------------
     tabItem(
       tabName = "analysis",
-      h2("Analysis"),
       box(
         title = "Movies",
         status = "primary",
         width = 6,
+        height = "500px",
         plotOutput(outputId = "scatter")
+      ),
+      tabBox(
+        width = 6,
+        height = "500px",
+        tabPanel(
+          title = "Movie info"
+        ),
+        tabPanel(
+          title = "Data Table",
+        ),
+        tabPanel(
+          title = "Model",
+        )
+      ),
+      box(
+        width = 12,
+        dataTableOutput("select_table")
       )
     )
 
