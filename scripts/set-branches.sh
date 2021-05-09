2ca903273371f97993adbb0cb1ffa2e06baadcb3
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2021-05-09 13:34:58 +0200

    upd scripts

--- scripts/set-branches.sh
+++ scripts/set-branches.sh
@@ -45,4 +45,12 @@ gcob 0.3 && greset 0.4^
 gcob 0.1 && greset 0.3^
 gcob 0.0 && greset 0.1^
 
-# git push dev --all -f
+git push dev --all -f
+
+# git push origin --all -f
+# git push origin :dev-slides
+
+
+
+
+
