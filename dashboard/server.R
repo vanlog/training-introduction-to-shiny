6f154739afa8a3686592eea28ced0f4728aa3cca
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2021-05-09 11:25:03 +0200

    1.8 Better style for datatable

--- dashboard/server.R
+++ dashboard/server.R
@@ -1,5 +1,12 @@
 server <- function(input, output, session) {
 
+  x_colour <- "darkred"
+  y_colour <- "darkgoldenrod"
+  marvel_color <- 'lightblue'
+  dc_color <- 'lightgreen'
+  dollar_bill_color <- "#85bb65"
+  budget_range <- range(df$Budget, df$Opening_Weekend_USA, df$Gross_USA, df$Gross_Worldwide)
+
   output$scatter <- renderPlot({
     flog.info("Rendering scatter...")
 
@@ -7,8 +14,10 @@ server <- function(input, output, session) {
       aes_string(x = "Budget", y = "Gross_Worldwide") +
       geom_point() +
       theme_bw() +
-      theme(axis.title.x = element_text(size = 18),
-            axis.title.y = element_text(size = 18))
+      theme(axis.title.x = element_text(colour = x_colour,
+                                        size = 18),
+            axis.title.y = element_text(colour = y_colour,
+                                        size = 18))
   })
 
   output$select_table <- renderDataTable({
@@ -25,7 +34,51 @@ server <- function(input, output, session) {
         pageLength = 3
       ),
       filter = list(position = 'top'),
-    )
+    ) %>%
+      # in-line bar
+      formatStyle(
+        'Rate',
+        background = styleColorBar(df$Rate, 'steelblue'),
+        backgroundSize = '90% 90%',
+        backgroundRepeat = 'no-repeat',
+        backgroundPosition = 'center'
+      ) %>%
+      # in-line bar
+      formatStyle(
+        'Metascore',
+        background = styleColorBar(df$Metascore, 'steelblue'),
+        backgroundSize = '90% 90%',
+        backgroundRepeat = 'no-repeat',
+        backgroundPosition = 'center'
+      ) %>%
+      # in-line bar
+      formatStyle(
+        'Minutes',
+        background = styleColorBar(df$Minutes, 'steelblue'),
+        backgroundSize = '90% 90%',
+        backgroundRepeat = 'no-repeat',
+        backgroundPosition = 'center'
+      ) %>%
+      # category coloured label
+      formatStyle(
+        'Company',
+        transform = 'rotateX(45deg) rotateY(0deg) rotateZ(0deg)',
+        backgroundColor = styleEqual(
+          unique(df$Company), c(marvel_color, dc_color)
+        )
+      ) %>%
+      # dollar bars with the same scales
+      formatStyle(
+        c("Budget", "Opening_Weekend_USA", "Gross_USA", "Gross_Worldwide"),
+        color = 'black',
+        background = styleColorBar(budget_range, dollar_bill_color),
+        backgroundSize = '100% 90%',
+        backgroundRepeat = 'no-repeat',
+        backgroundPosition = 'center'
+      ) %>%
+      # highlight scatter variables
+      formatStyle("Budget",  color = x_colour, fontWeight = 'bold') %>%
+      formatStyle("Gross_Worldwide",  color = y_colour, fontWeight = 'bold')
   })
 
 }
